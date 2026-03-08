e41092fe3aec3665da82d6968e299b02ee7a5c1f
Anshul <mishraanshul559@gmail.com>
2026-03-08 11:54:52 +0530

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+
+
 export default defineConfig({
   base: "/",
   plugins: [react()],
